b187d12bfc1c4a4fa6d7645da282d722b269a8e3
lenovo <lenovo@LAPTOP-M0PADGFH>
2017-04-06 23:08:17 +0800

    增加了单元测试

--- src/main/java/com/binli/agiledev/controller/CoreTableColumnCtl.java
+++ src/main/java/com/binli/agiledev/controller/CoreTableColumnCtl.java
@@ -3,6 +3,9 @@ package com.binli.agiledev.controller;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -25,7 +28,7 @@ public class CoreTableColumnCtl extends BaseCtl{
 	@RequestMapping("/aa")
 	public String aa(@RequestBody List<TCoreColumn> requestBody) { 
 		
-		TCoreTableExt tCoreTableExt=new TCoreTableExt();
+		/*TCoreTableExt tCoreTableExt=new TCoreTableExt();
 		tCoreTableExt.setComment("注释");
 		tCoreTableExt.setCreTim(new Date());
 		tCoreTableExt.setDbId("1");
@@ -33,11 +36,32 @@ public class CoreTableColumnCtl extends BaseCtl{
 		tCoreTableExt.settCoreColumn(requestBody );
 		
 		
-		iCoreTableService.add(tCoreTableExt);
+		iCoreTableService.add(tCoreTableExt);*/
 		
 		
 		System.out.println(iCoreTableService);
 		System.out.println(requestBody.size());
+		
+		return null;
+	}
+	@ResponseBody
+	@RequestMapping("/b")
+	public String  b(HttpServletRequest request,  
+            HttpServletResponse response  ) { 
+		
+		/*TCoreTableExt tCoreTableExt=new TCoreTableExt();
+		tCoreTableExt.setComment("注释");
+		tCoreTableExt.setCreTim(new Date());
+		tCoreTableExt.setDbId("1");
+		tCoreTableExt.setTabName("users");
+		tCoreTableExt.settCoreColumn(requestBody );
+		
+		
+		iCoreTableService.add(tCoreTableExt);*/
+		
+		
+		System.out.println(iCoreTableService);
+		 
 		
 		return null;
 	}
---
+++ src/test/java/com/binli/agiledev/controller/TestCoreTableColumnCtl.java
@@ -0,0 +1,58 @@
+package com.binli.agiledev.controller;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.binli.agiledev.service.table.ICoreTableService;
+
+
+/**
+ * @author  
+ * 2015-4-7
+ */
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration({"classpath*:/applicationContext.xml","classpath*:/springmvc-servlet.xml"})
+public class TestCoreTableColumnCtl {
+	// 模拟request,response  
+    private MockHttpServletRequest request;  
+    private MockHttpServletResponse response;   
+      
+    // 注入loginController  
+    @Autowired  
+    private CoreTableColumnCtl loginController ;  
+    
+    @Autowired
+	public ICoreTableService iCoreTableService;
+    // 执行测试方法之前初始化模拟request,response  
+    @Before    
+    public void setUp(){    
+        request = new MockHttpServletRequest();      
+        request.setCharacterEncoding("UTF-8");      
+        response = new MockHttpServletResponse();      
+    }         
+    /** 
+     *  
+     * @Title：testLogin 
+     * @Description: 测试用户登录   
+     */  
+    @Test  
+    public void testLogin() {   
+        try {  
+        	request.setParameter("userName", "admin");
+        	request.setParameter("password", "2");
+           assertEquals("login",loginController.b(request, response)) ;  
+        	System.out.println(iCoreTableService);
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+}

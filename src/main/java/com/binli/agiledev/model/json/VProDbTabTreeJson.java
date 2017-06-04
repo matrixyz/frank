7b22c7e97ebc72b7da81ff78f49b3d3680c5ee3a
lenovo <lenovo@LAPTOP-M0PADGFH>
2017-06-04 22:18:16 +0800

    a

--- src/main/java/com/binli/agiledev/model/json/VProDbTabTreeJson.java
+++ src/main/java/com/binli/agiledev/model/json/VProDbTabTreeJson.java
@@ -3,6 +3,11 @@ package com.binli.agiledev.model.json;
 import java.util.List;
 
 import com.binli.agiledev.dao.model.VProDbTabTree;
+/**
+ * 该类是为了适应easyui treegrid的 要求的数据格式
+ * @author lenovo
+ *
+ */
 
 public class VProDbTabTreeJson {
 	private int total;
